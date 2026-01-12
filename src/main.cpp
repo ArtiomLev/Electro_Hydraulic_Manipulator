b958461f8519ea0d6dde200bf9a72d9488a27137
artiom <artiomlevhykov@gmail.com>
2026-01-12 05:17:08 +0300

    [chore]: Add shift reg i/o lib init

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 
 #include "port.h"
 
+#include "GyverShift.h"
+GyverShift<OUTPUT, 1> reg_in(SHIFT_OUT_UPD, SHIFT_OUT_DATA, SHIFT_OUT_CLK);
+GyverShift<INPUT, 2> reg_out(SHIFT_IN_PL, SHIFT_IN_DATA, SHIFT_IN_CLK);
+
 void setup() {
 
 }
