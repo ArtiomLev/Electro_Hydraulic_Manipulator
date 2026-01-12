0cf2e46ed94732eb6ea3a9be765c4a8420f5db2b
artiom <artiomlevhykov@gmail.com>
2026-01-12 05:26:32 +0300

    [chore]: Add steppers objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 
 #include "port.h"
+#include "hardwareParameters.h"
 
 #include "GyverShift.h"
 GyverShift<OUTPUT, 1> reg_in(SHIFT_OUT_UPD, SHIFT_OUT_DATA, SHIFT_OUT_CLK);
 GyverShift<INPUT, 2> reg_out(SHIFT_IN_PL, SHIFT_IN_DATA, SHIFT_IN_CLK);
+
+#include "GyverStepper2.h"
+GStepper2<STEPPER2WIRE> stepper1(MOTOR1_STEP_PER_ROUND, DRV1_STP, DRV1_DIR);
+GStepper2<STEPPER2WIRE> stepper2(MOTOR2_STEP_PER_ROUND, DRV2_STP, DRV2_DIR);
+GStepper2<STEPPER2WIRE> stepper3(MOTOR3_STEP_PER_ROUND, DRV3_STP, DRV3_DIR);
+GStepper2<STEPPER2WIRE> stepper4(MOTOR4_STEP_PER_ROUND, DRV4_STP, DRV4_DIR);
+GStepper2<STEPPER2WIRE> stepper5(MOTOR5_STEP_PER_ROUND, DRV5_STP, DRV5_DIR);
 
 void setup() {
 
