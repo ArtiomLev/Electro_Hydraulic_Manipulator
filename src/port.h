5d144e30f88c128a3325666f575e452c64374ec8
artiom <artiomlevhykov@gmail.com>
2026-01-12 04:44:32 +0300

    [chore]: Add define aliases for used GPIO pins

---
+++ src/port.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// Индикация
+
+#define BUILTIN_LED 2
+#define BUZZER 13
+
+// Ввод-вывод
+
+#define SHIFT_IN_PL 33
+#define SHIFT_IN_CLK 32
+#define SHIFT_IN_DATA 35
+
+#define SHIFT_OUT_CLK 4
+#define SHIFT_OUT_DATA 15
+#define SHIFT_OUT_UPD 5
+
+// Драйверы
+
+#define DRV_EN 21
+
+#define DRV1_STP 25
+#define DRV1_DIR 26
+
+#define DRV2_STP 27
+#define DRV2_DIR 14
+
+#define DRV3_STP 23
+#define DRV3_DIR 22
+
+#define DRV4_STP 19
+#define DRV4_DIR 18
+
+#define DRV5_STP 17
+#define DRV5_DIR 16
